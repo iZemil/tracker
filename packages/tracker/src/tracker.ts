59b5de12f2d21e94ae5345b885adc38fba870d5a
zemil <3zemil@gmail.com>
2023-02-19 13:38:14 +0300

    fix(tracker): timer logic

--- packages/tracker/src/tracker.ts
+++ packages/tracker/src/tracker.ts
@@ -2,13 +2,13 @@ import { api, ITrack } from './utils';
 
 export class Tracker {
 	private buffer: ITrack[];
-	private lastRequestAt = 0;
+	private timer?: ReturnType<typeof setTimeout>;
 
 	constructor(buffer: ITrack[] = []) {
 		this.buffer = buffer;
 
 		window.addEventListener('beforeunload', () => {
-			this.tryClear(true);
+			this.clear();
 		});
 	}
 
@@ -18,41 +18,36 @@ export class Tracker {
 			tags,
 			url: window.location.href,
 			title: document.title,
-			ts: this.timestamp,
+			ts: Math.floor(Date.now() / 1000),
 		});
 	}
 
-	private get timestamp(): number {
-		return Math.floor(Date.now() / 1000);
+	private updateTimer(cb: () => void, ttl = 1000) {
+		this.timer = setTimeout(() => {
+			this.timer = undefined;
+			cb();
+		}, ttl);
 	}
 
 	private append(...payload: ITrack[]) {
 		this.buffer.push(...payload);
-		this.tryClear();
+
+		if (this.buffer.length >= 3) {
+			this.clear();
+		} else if (!this.timer) {
+			this.updateTimer(() => this.clear());
+		}
 	}
 
-	private async tryClear(isForce = false) {
-		const ttl = this.timestamp - this.lastRequestAt;
-		const isValidTimeout = ttl > 1;
-		const isMinimumSize = this.buffer.length >= 3;
-		const isValidSize = this.buffer.length > 0;
-		const canTry = isValidSize && (isValidTimeout || isMinimumSize || isForce);
-
-		if (canTry) {
+	private async clear() {
+		if (this.buffer.length > 0) {
 			const pending = [...this.buffer];
 			this.buffer = [];
-			this.lastRequestAt = this.timestamp;
 
-			let ok = false;
 			try {
-				const res = await api.sendTrack(pending);
-				ok = res.ok;
-			} catch (err) {
-				console.warn('request', err);
-			}
-
-			if (!ok) {
-				this.append(...pending);
+				await api.sendTrack(pending);
+			} catch {
+				this.updateTimer(() => this.append(...pending));
 			}
 		}
 	}
